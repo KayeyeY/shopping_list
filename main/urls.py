ce6cafe57191d92b1a49827de069948d2a3c5da7
Kevin Manurung <kevinmanurung0207@gmail.com>
2023-09-20 15:47:12 +0700

    tutorial 3

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,7 @@ from main.views import create_product
 from main.views import show_xml
 from main.views import show_json
 from main.views import show_xml_by_id, show_json_by_id 
+from main.views import register, login_user, logout_user
 
 app_name = 'main'
 
@@ -14,4 +15,7 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
